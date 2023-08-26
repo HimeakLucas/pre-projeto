099e5263ecc4d2290ef6b664d93ee2cc5c93780f
Lucas <lucasadeodato.oli@gmail.com>
2023-08-26 16:12:24 -0300

    Adicionando modules(categoria/terefa)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TarefaModule } from './modules/tarefa/tarefa.module';
+import { CategoriaModule } from './modules/categoria/categoria.module';
 
 @Module({
-  imports: [],
+  imports: [TarefaModule, CategoriaModule],
   controllers: [AppController],
   providers: [AppService],
 })
